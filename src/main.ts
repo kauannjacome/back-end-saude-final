e6ee8fc5733dcd3c20d90b994e247edfb1c13bd3
kauannjacome <kauannjacome@hotmail.com>
2025-11-11 11:00:09 -0300

    validando antes do serviços

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,7 +9,13 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: false, // deve ser false se origin for '*'
   });
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,            // Remove campos que não estão no DTO
+      forbidNonWhitelisted: true, // (Opcional) Lança erro se vier campo que não está no DTO
+      transform: true,            // Converte tipos (string para number, etc)
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
