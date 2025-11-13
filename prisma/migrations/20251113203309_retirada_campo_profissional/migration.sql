bf8ae5dab918ec51cdd96b2e06023d592a26db07
kauannjacome <kauannjacome@hotmail.com>
2025-11-13 17:37:17 -0300

    criação de seeds

---
+++ prisma/migrations/20251113203309_retirada_campo_profissional/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `professional_name` on the `professional` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "professional" DROP COLUMN "professional_name";
