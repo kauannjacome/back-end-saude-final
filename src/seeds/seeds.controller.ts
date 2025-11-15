e949349757a85b04fffda2bb80385243adc7d7f4
kauannjacome <kauannjacome@hotmail.com>
2025-11-15 16:48:43 -0300

    adição  seeds 3

--- src/seeds/seeds.controller.ts
+++ src/seeds/seeds.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Logger, UploadedFile, ParseFilePipeBuilder, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, Logger, UploadedFile,  BadRequestException } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
@@ -11,30 +11,32 @@ export class SeedsController {
  private readonly logger = new Logger(SeedsController.name);
 
 
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: memoryStorage(),
-      limits: { fileSize: 10 * 1024 * 1024 },
-    }),
-  )
-  @Post('/person')
-  csvPerson(
-    @Body() CreateSeedPatient: CreateSeedPatientDto,
-    @UploadedFile(
-      new ParseFilePipeBuilder()
-        .addFileTypeValidator({ fileType: /(text\/csv|application\/vnd\.ms-excel)$/ })
-        .build({
-          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-        }),
-    )
-    file: Express.Multer.File,
-  ) {
-    this.logger.log(
-      `Recebido upload CSV — payload=${JSON.stringify(
-        CreateSeedPatient,
-      )} file=${file.originalname}`,
-    );
-
-    return this.seedsService.csvPerson(CreateSeedPatient, file);
+@UseInterceptors(
+  FileInterceptor('file', {
+    storage: memoryStorage(),
+    limits: { fileSize: 10 * 1024 * 1024 },
+  }),
+)
+@Post('/person')
+csvPerson(
+  @Body() CreateSeedPatient: CreateSeedPatientDto,
+  @UploadedFile() file: Express.Multer.File,
+) {
+  if (!file) {
+    throw new BadRequestException('Arquivo CSV obrigatório');
   }
+
+  if (!file.originalname.toLowerCase().endsWith('.csv')) {
+    throw new BadRequestException('O arquivo deve ser um .csv');
+  }
+
+  this.logger.log(
+    `Recebido upload CSV — payload=${JSON.stringify(
+      CreateSeedPatient,
+    )} file=${file.originalname}`,
+  );
+
+  return this.seedsService.csvPerson(CreateSeedPatient, file);
+}
+
 }
