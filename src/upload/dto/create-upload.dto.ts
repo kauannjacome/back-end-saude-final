27ffe8e0f0f1cec79ff450308e86f63596be915e
kauannjacome <kauannjacome@hotmail.com>
2025-11-15 08:04:04 -0300

    adicionar validação

--- src/upload/dto/create-upload.dto.ts
+++ src/upload/dto/create-upload.dto.ts
@@ -10,5 +10,6 @@ export class CreateUploadDto {
     @IsNotEmpty({ message: 'O tipo é obrigatório.' })
   @IsEnum(TipoArquivo, { message: 'O tipo deve ser estadual, municipal ou federal.' })
   tipo: TipoArquivo;
+    @IsNotEmpty({ message: 'O id é obrigatório.' })
   id:number;
 }
