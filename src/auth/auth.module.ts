445a5caaa09d2ab1732fa447bb8a9fe686e8f37d
kauannjacome <kauannjacome@hotmail.com>
2026-01-02 00:39:39 -0300

    login 3

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { HashingServiceProtocol } from './hash/hashing.service';
 import { BcryptService } from './hash/bcrypt.service';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 // Módulo global - Pode ser usado na aplicação inteira ( não precisa importar em outros módulos pra usar )
 @Global()
 @Module({
+  imports: [PrismaModule],
   providers: [
     {
       provide: HashingServiceProtocol,
